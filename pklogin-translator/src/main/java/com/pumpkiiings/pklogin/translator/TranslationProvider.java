a21ed7a6cf1d786f1091c8fc72e07d04011d3a8e
Pumpkiiiings <pklogin@pumpkiiings.com>
2026-08-24 18:37:11 -0600

    Remove ignored folders from repository

--- pklogin-translator/src/main/java/com/pumpkiiings/pklogin/translator/TranslationProvider.java
+++
@@ -1,5 +0,0 @@
-package com.pumpkiiings.pklogin.translator;
-
-public interface TranslationProvider {
-    String translate(String text, String sourceLanguage, String targetLanguage) throws Exception;
-}
